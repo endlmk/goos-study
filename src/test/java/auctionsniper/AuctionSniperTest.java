1d1ffbf840b13e93c042feea7c04d5235939cc72
endlmk <1269467+endlmk@users.noreply.github.com>
2022-07-30 22:50:10 +0900

    Done 14.4.

--- src/test/java/auctionsniper/AuctionSniperTest.java
+++ src/test/java/auctionsniper/AuctionSniperTest.java
@@ -1,19 +1,30 @@
 package auctionsniper;
 
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import static auctionsniper.AuctionEventListener.*;
+import static auctionsniper.AuctionEventListener.PriceSource;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AuctionSniperTest {
     private final Auction auction = Mockito.mock(Auction.class);
-    private final SniperListener sniperListener = Mockito.mock(SniperListener.class);
+    private final SniperListener sniperListener = Mockito.spy(new SniperListenerStub());
     private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
-
+    private enum SniperStateForSpy { Lost, Bidding, Winning }
+    private SniperStateForSpy state = SniperStateForSpy.Lost;
     @Test
-    public void reportsLostWhenAuctionClosed() {
+    public void reportsLostWhenAuctionClosedImmediately() {
         sniper.auctionClosed();
         Mockito.verify(sniperListener).sniperLost();
+    }
+
+    @Test
+    public void reportsLostIfAuctionClosesWhenBidding() {
+        sniper.currentPrice(123,45, PriceSource.FromOtherBidder);
+        sniper.auctionClosed();
+        Mockito.verify(sniperListener).sniperLost();
+        assertThat(state, IsEqual.equalTo(SniperStateForSpy.Bidding));
     }
 
     @Test
@@ -30,5 +41,22 @@ public class AuctionSniperTest {
     public void reportsIsWinningWhenCurrentPriceComesFromSniper() {
         sniper.currentPrice(123, 45, PriceSource.FromSniper);
         Mockito.verify(sniperListener).sniperWinning();
+    }
+
+    private class SniperListenerStub implements SniperListener {
+        @Override
+        public void sniperLost() {
+
+        }
+
+        @Override
+        public void sniperBidding() {
+            state = SniperStateForSpy.Bidding;
+        }
+
+        @Override
+        public void sniperWinning() {
+
+        }
     }
 }
