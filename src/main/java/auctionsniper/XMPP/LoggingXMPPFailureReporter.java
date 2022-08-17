eeaf3d286393c0c9415b185ee4bf8bed7c88b1b3
endlmk <1269467+endlmk@users.noreply.github.com>
2022-08-17 22:38:55 +0900

    Doing 19.5. Passed logging test.

--- src/main/java/auctionsniper/XMPP/LoggingXMPPFailureReporter.java
+++ src/main/java/auctionsniper/XMPP/LoggingXMPPFailureReporter.java
@@ -4,11 +4,14 @@ import java.util.logging.Logger;
 
 public class LoggingXMPPFailureReporter implements XMPPFailureReporter {
 
-    public LoggingXMPPFailureReporter(Logger logger) {
+    private final Logger logger;
+    private static final String MESSAGE_FORMAT = "<%s> Could not translate message \"%s\" because \"%s\"";
 
+    public LoggingXMPPFailureReporter(Logger logger) {
+        this.logger = logger;
     }
     @Override
     public void cannotTranslateMessage(String auctionId, String failedMessage, Exception exception) {
-
+        logger.severe(String.format(MESSAGE_FORMAT, auctionId, failedMessage, exception.toString()));
     }
 }
